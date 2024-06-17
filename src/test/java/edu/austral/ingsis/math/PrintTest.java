4b55a33a4723073469667148f789d0a79a5b76df
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-06-17 19:50:32 -0300

    print tests

--- src/test/java/edu/austral/ingsis/math/PrintTest.java
+++ src/test/java/edu/austral/ingsis/math/PrintTest.java
@@ -3,15 +3,19 @@ package edu.austral.ingsis.math;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.austral.ingsis.math.Visitor.PrintVisitor;
 import org.junit.jupiter.api.Test;
 
 public class PrintTest {
 
+  PrintVisitor printVisitor = new PrintVisitor();
+
   /** Case 1 + 6 */
   @Test
   public void shouldPrintFunction1() {
-    final String expected = "1 + 6";
-    final String result = expected;
+    final String expected = "(1.0 + 6.0)";
+    Addition addition = new Addition(new Value(1), new Value(6));
+    final String result = printVisitor.visitAddition(addition);
 
     assertThat(result, equalTo(expected));
   }
@@ -19,8 +23,9 @@ public class PrintTest {
   /** Case 12 / 2 */
   @Test
   public void shouldPrintFunction2() {
-    final String expected = "12 / 2";
-    final String result = expected;
+    final String expected = "(12.0 / 2.0)";
+    Function function = new Division(new Value(12), new Value(2));
+    final String result = printVisitor.visitDivision((Division) function);
 
     assertThat(result, equalTo(expected));
   }
@@ -28,8 +33,9 @@ public class PrintTest {
   /** Case (9 / 2) * 3 */
   @Test
   public void shouldPrintFunction3() {
-    final String expected = "(9 / 2) * 3";
-    final String result = expected;
+    final String expected = "((9.0 / 2.0) * 3.0)";
+    Function function = new Multiplication(new Division(new Value(9), new Value(2)), new Value(3));
+    final String result = printVisitor.visitMultiplication((Multiplication) function);
 
     assertThat(result, equalTo(expected));
   }
@@ -37,8 +43,9 @@ public class PrintTest {
   /** Case (27 / 6) ^ 2 */
   @Test
   public void shouldPrintFunction4() {
-    final String expected = "(27 / 6) ^ 2";
-    final String result = expected;
+    final String expected = "((27.0 / 6.0) ^ 2.0)";
+    Function function = new Power(new Division(new Value(27), new Value(6)), new Value(2));
+    final String result = printVisitor.visitPower((Power) function);
 
     assertThat(result, equalTo(expected));
   }
@@ -46,8 +53,10 @@ public class PrintTest {
   /** Case |value| - 8 */
   @Test
   public void shouldPrintFunction6() {
-    final String expected = "|value| - 8";
-    final String result = expected;
+    final String expected = "(|value| - 8.0)";
+    Function absolute = new Absolut(new Variable("value"));
+    Function function = new Subtract(absolute, new Value(8));
+    final String result = printVisitor.visitSubtract((Subtract) function);
 
     assertThat(result, equalTo(expected));
   }
@@ -55,8 +64,10 @@ public class PrintTest {
   /** Case |value| - 8 */
   @Test
   public void shouldPrintFunction7() {
-    final String expected = "|value| - 8";
-    final String result = expected;
+    final String expected = "(|value| - 8.0)";
+    Function absolute = new Absolut(new Variable("value"));
+    Function function = new Subtract(absolute, new Value(8));
+    final String result = printVisitor.visitSubtract((Subtract) function);
 
     assertThat(result, equalTo(expected));
   }
@@ -64,8 +75,9 @@ public class PrintTest {
   /** Case (5 - i) * 8 */
   @Test
   public void shouldPrintFunction8() {
-    final String expected = "(5 - i) * 8";
-    final String result = expected;
+    final String expected = "((5.0 - i) * 8.0)";
+    Function function = new Multiplication(new Subtract(new Value(5), new Variable("i")), new Value(8));
+    final String result = printVisitor.visitMultiplication((Multiplication) function);
 
     assertThat(result, equalTo(expected));
   }
