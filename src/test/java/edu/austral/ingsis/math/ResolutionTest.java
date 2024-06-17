88ab56becc6b707751fb8b7cd74950827bafb4c8
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-06-17 20:02:42 -0300

    resolution tests and added sqr root

--- src/test/java/edu/austral/ingsis/math/ResolutionTest.java
+++ src/test/java/edu/austral/ingsis/math/ResolutionTest.java
@@ -3,71 +3,93 @@ package edu.austral.ingsis.math;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.austral.ingsis.math.Visitor.CalculateVisitor;
 import org.junit.jupiter.api.Test;
 
 public class ResolutionTest {
 
+  CalculateVisitor calculateVisitor = new CalculateVisitor();
+
   /** Case 1 + 6 */
   @Test
   public void shouldResolveSimpleFunction1() {
-    final Double result = 7d;
+    Addition addition = new Addition(new Value(1), new Value(6));
+    Double result = addition.accept(calculateVisitor);
 
-    assertThat(result, equalTo(7d));
+    assertThat(result, equalTo(7.0));
   }
 
   /** Case 12 / 2 */
   @Test
   public void shouldResolveSimpleFunction2() {
-    final Double result = 6d;
+    Division division = new Division(new Value(12), new Value(2));
+    Double result = division.accept(calculateVisitor);
 
-    assertThat(result, equalTo(6d));
+    assertThat(result, equalTo(6.0));
   }
 
   /** Case (9 / 2) * 3 */
   @Test
   public void shouldResolveSimpleFunction3() {
-    final Double result = 13.5;
+    Multiplication multiplication =
+        new Multiplication(new Division(new Value(9), new Value(2)), new Value(3));
+    final Double result = multiplication.accept(calculateVisitor);
 
-    assertThat(result, equalTo(13.5d));
+    assertThat(result, equalTo(13.5));
   }
 
   /** Case (27 / 6) ^ 2 */
   @Test
   public void shouldResolveSimpleFunction4() {
-    final Double result = 20.25;
+    Power power = new Power(new Division(new Value(27), new Value(6)), new Value(2));
+    final Double result = power.accept(calculateVisitor);
 
-    assertThat(result, equalTo(20.25d));
+    assertThat(result, equalTo(20.25));
   }
 
   /** Case 36 ^ (1/2) */
   @Test
   public void shouldResolveSimpleFunction5() {
-    final Double result = 6d;
+    Power power = new Power(new Value(36), new Division(new Value(1), new Value(2)));
+    final Double result = power.accept(calculateVisitor);
 
-    assertThat(result, equalTo(6d));
+    assertThat(result, equalTo(6.0));
   }
 
   /** Case |136| */
   @Test
   public void shouldResolveSimpleFunction6() {
-    final Double result = 136d;
+    Absolut absolute = new Absolut(new Value(136));
+    final Double result = absolute.accept(calculateVisitor);
 
-    assertThat(result, equalTo(136d));
+    assertThat(result, equalTo(136.0));
   }
 
   /** Case |-136| */
   @Test
   public void shouldResolveSimpleFunction7() {
-    final Double result = 136d;
+    Absolut absolute = new Absolut(new Value(-136));
+    final Double result = absolute.accept(calculateVisitor);
 
-    assertThat(result, equalTo(136d));
+    assertThat(result, equalTo(136.0));
   }
 
   /** Case (5 - 5) * 8 */
   @Test
   public void shouldResolveSimpleFunction8() {
-    final Double result = 0d;
+    Subtract subtract = new Subtract(new Value(5), new Value(5));
+    Multiplication multiplication = new Multiplication(subtract, new Value(8));
+    final Double result = multiplication.accept(calculateVisitor);
 
-    assertThat(result, equalTo(0d));
+    assertThat(result, equalTo(0.0));
+  }
+
+  /** Case sqrt(25) */
+  @Test
+  public void shouldResolveSimpleFunction9() {
+    SquareRoot squareRoot = new SquareRoot(new Value(25));
+    final Double result = squareRoot.accept(calculateVisitor);
+
+    assertThat(result, equalTo(5.0));
   }
 }
